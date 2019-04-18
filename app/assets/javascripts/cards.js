fd0ecf80c9b92e208b8e98ff3ed875799818a3af
Joseph Papaluca <jpapaluc@gmail.com>
2019-04-17 22:09:17 -0400

    added AJAX calls

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,3 +1,20 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function() {
+  $('#card-search-form').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: $(this).attr('method'),
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      datatype: 'json'
+    }).done(function(data) {
+      $('card-results').prepend(data)
+    }).fail(function(data) {
+      console.log("didn't work");
+    }).always(function() {
+      console.log("AJAX did a thing");
+    });
+  });
+});
